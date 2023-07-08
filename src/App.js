7abab9436bb57987d6acf9dcdd17464a2c50d638
DucVan <nguyenducvan260903@gmail.com>
2023-07-08 17:59:49 +0700

    Cấu hình layout/ router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Fragment } from 'react';
+// import Home from './pages/Home';
+// import Following from './pages/Following';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {/* <Route path='/' element={<Home />}/>
+                    <Route path='/following' element={<Following />}/> */}
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if(route.layout) {
+                            Layout = route.layout;
+                        console.log(Layout);
+                        } else if(route.layout === null){
+                            Layout = Fragment;
+                        }
+                        const Page = route.component;//chuyen tu component sang element
+                        return <Route key={index} path={route.path} element={<Layout><Page /></Layout>}/>
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
